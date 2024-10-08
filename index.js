1e34c2a903fdcfda15b652b2f6eb7518f00c874a
JOEL <joelgaye17@gmail.com>
2024-10-08 18:23:30 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);  // Modifies the original array
+    return cats;
+  }
+  function destructivelyPrependCat(name) {
+    cats.unshift(name);  // Modifies the original array
+    return cats;
+  }
+  function destructivelyRemoveLastCat() {
+    cats.pop();  // Modifies the original array
+    return cats;
+  }
+  function destructivelyRemoveFirstCat() {
+    cats.shift();  // Modifies the original array
+    return cats;
+  }
+  function appendCat(name) {
+    return [...cats, name];  // Returns a new array, leaves the original unchanged
+  }
+  function prependCat(name) {
+    return [name, ...cats];  // Returns a new array, leaves the original unchanged
+  }
+  function removeLastCat() {
+    return cats.slice(0, cats.length - 1);  // Returns a new array, leaves the original unchanged
+  }
+  function removeFirstCat() {
+    return cats.slice(1);  // Returns a new array, leaves the original unchanged
+  }
+          
